0fb74be7b308aea8639fbef43edaae1122603b47
Nikita Kirin <kirinnikita1406@gmail.com>
2023-11-18 22:06:51 +0500

    [DEV] Laravel-lang package implementation
    https://github.com/Laravel-Lang/common

---
+++ lang/en/crud.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'messages' => [
+        'create' => [
+            'success' => 'Entry created successfully',
+            'fail' => 'An error occurred while creating the entry',
+        ],
+        'update' => [
+            'success' => 'Entry updated successfully',
+            'fail' => 'An error occurred while updating the entry',
+        ],
+    ],
+];
