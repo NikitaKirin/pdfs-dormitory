e2275408dcd431229159b1e29fc7b849c5089b7a
Nikita Kirin <kirinnikita1406@gmail.com>
2023-12-16 20:54:41 +0500

    [DEV] User's id in API resource
    https://yougile.com/team/28c1a6d0ae9a/#POD-12

--- app/Http/Resources/V1/User/UserResource.php
+++ app/Http/Resources/V1/User/UserResource.php
@@ -23,6 +23,7 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
             'roles' => RoleResource::collection($this->whenLoaded('roles')),
